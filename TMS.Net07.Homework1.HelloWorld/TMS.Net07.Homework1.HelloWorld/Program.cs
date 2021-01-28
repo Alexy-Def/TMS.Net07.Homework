8f0c61463c2799c3ba4a1aa46b7aa39a882d52b8
Jok <sasha.golubovich@tut.by>
2021-01-28 22:22:54 +0300

    Middle level

--- TMS.Net07.Homework1.HelloWorld/TMS.Net07.Homework1.HelloWorld/Program.cs
+++ TMS.Net07.Homework1.HelloWorld/TMS.Net07.Homework1.HelloWorld/Program.cs
@@ -10,8 +10,22 @@ namespace TMS.Net07.Homework1.HelloWorld
         {
             Console.Write("Введите, пожалуйста, какое-нибудь число: ");
             string anyNumber = Console.ReadLine();
-            int number = int.Parse(anyNumber);
-            Console.WriteLine($"Введенное Вами число: {number}");
+            Console.WriteLine();
+            bool validation = int.TryParse(anyNumber, out int parseNumber);
+
+            if (validation == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Отлично! Введенное Вами число является действительным. И оно равно {0}", parseNumber);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("К сожалению, скорее всего, Вы ввели не число.");
+                Console.ResetColor();
+            }
+
             Console.WriteLine();
             Console.WriteLine("...Нажмите любую клавишу для выхода из программы...");
             Console.ReadKey();
