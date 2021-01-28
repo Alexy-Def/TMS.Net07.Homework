a0c24553dac5a051c258e0849dcb161a541a116d
Jok <sasha.golubovich@tut.by>
2021-01-28 22:18:42 +0300

    Low level

---
+++ TMS.Net07.Homework1.HelloWorld/TMS.Net07.Homework1.HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// Консольное приложение, которое выводит введенное пользователем число на экран
+
+using System;
+
+namespace TMS.Net07.Homework1.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите, пожалуйста, какое-нибудь число: ");
+            string anyNumber = Console.ReadLine();
+            int number = int.Parse(anyNumber);
+            Console.WriteLine($"Введенное Вами число: {number}");
+            Console.WriteLine();
+            Console.WriteLine("...Нажмите любую клавишу для выхода из программы...");
+            Console.ReadKey();
+        }
+    }
+}
