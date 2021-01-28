c36f3123bbff39cdca7fe5246d48eadaddb0ec50
Jok <sasha.golubovich@tut.by>
2021-01-28 22:29:14 +0300

    High level

--- TMS.Net07.Homework1.HelloWorld/TMS.Net07.Homework1.HelloWorld/Program.cs
+++ TMS.Net07.Homework1.HelloWorld/TMS.Net07.Homework1.HelloWorld/Program.cs
@@ -1,6 +1,8 @@
-﻿// Консольное приложение, которое выводит введенное пользователем число на экран
+﻿// Консольное приложение, которое выводит введенное пользователем число на экран, при этом запрашивая ввод следующего числа.
+// При вводе 0, программа должна закрыться сама.
 
 using System;
+using System.Threading;
 
 namespace TMS.Net07.Homework1.HelloWorld
 {
@@ -8,6 +10,7 @@ namespace TMS.Net07.Homework1.HelloWorld
     {
         static void Main(string[] args)
         {
+            begin:
             Console.Write("Введите, пожалуйста, какое-нибудь число: ");
             string anyNumber = Console.ReadLine();
             Console.WriteLine();
@@ -15,6 +18,14 @@ namespace TMS.Net07.Homework1.HelloWorld
 
             if (validation == true)
             {
+                if (parseNumber == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.Write("Из-за числа '0' программа вынуждена сама закрыться через несколько секунд...");
+                    Thread.Sleep(3000);
+                    Environment.Exit(0);
+                }
+
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Отлично! Введенное Вами число является действительным. И оно равно {0}", parseNumber);
                 Console.ResetColor();
@@ -27,8 +38,7 @@ namespace TMS.Net07.Homework1.HelloWorld
             }
 
             Console.WriteLine();
-            Console.WriteLine("...Нажмите любую клавишу для выхода из программы...");
-            Console.ReadKey();
+            goto begin;
         }
     }
 }
